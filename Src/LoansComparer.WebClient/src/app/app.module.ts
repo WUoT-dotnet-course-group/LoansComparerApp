c9078d5d94e7ffaf32ca3bb48ba51957d0da286c
Jakub Urbański <kuba.urban01@gmail.com>
2022-11-27 01:46:43 +0100

    LOAN-5: add app description column

--- Src/LoansComparer.WebClient/src/app/app.module.ts
+++ Src/LoansComparer.WebClient/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, HomeComponent, LayoutComponent],
@@ -28,6 +29,7 @@ import { HeaderComponent } from './header/header.component';
     MatIconModule,
     MatListModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
