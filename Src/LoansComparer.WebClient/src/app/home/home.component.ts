6aec8ffe4822b7cfad05b3be6a4b69ade1442208
Jakub Urbański <kuba.urban01@gmail.com>
2022-11-27 21:08:16 +0100

    LOAN-5: add animated app description

--- Src/LoansComparer.WebClient/src/app/home/home.component.ts
+++ Src/LoansComparer.WebClient/src/app/home/home.component.ts
@@ -1,12 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.less'],
+  animations: [
+    trigger('toggleLearnMore', [
+      state('*', style({ opacity: 1 })),
+      state('void', style({ opacity: 0 })),
+      transition('* => void', [animate('0.5s ease-in')]),
+      transition('void => *', [animate('0.5s 0.5s ease-in')]),
+    ]),
+  ],
 })
 export class HomeComponent implements OnInit {
+  isLearnMore: boolean = true;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onLearnMoreClick(): void {
+    this.isLearnMore = !this.isLearnMore;
+  }
 }
