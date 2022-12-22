e4aa3f6a7bfae8b75afe9e870853467a8fb54786
josovva <josovva@users.noreply.github.com>
2022-12-22 18:01:36 +0000

    Automated dotnet-format update Co-authored-by: josovva

--- Src/Core/LoansComparer.Domain/Entities/Inquiry.cs
+++ Src/Core/LoansComparer.Domain/Entities/Inquiry.cs
@@ -7,10 +7,10 @@
         public short NumberOfInstallments { get; set; }
         public DateTime InquireDate { get; set; }
         public Guid ChosenBankInquiryId { get; set; }
-        
+
         public Guid UserID { get; set; }
         public User User { get; set; } = default!;
-        
+
         public Guid? ChosenBankID { get; set; }
         public Bank? ChosenBank { get; set; } = default!;
     }
