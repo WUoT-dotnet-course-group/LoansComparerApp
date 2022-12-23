5ddd22f0415f7994986b7d0b2dcb48ace28bd8ff
Jakub Urbański <kuba.urban01@gmail.com>
2022-12-23 01:07:53 +0100

    LOAN-12: add google auth configuration mapping

---
+++ Src/LoansComparer.WebAPI/Configuration/GoogleAuthConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace LoansComparer.WebAPI.Configuration
+{
+    internal class GoogleAuthConfig
+    {
+        public const string SectionName = "GoogleAuth";
+
+        public string ClientId { get; set; } = string.Empty;
+        public string SecretKey { get; set; } = string.Empty;
+    }
+}
