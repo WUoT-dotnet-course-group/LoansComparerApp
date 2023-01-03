4b29021cb65864b773df408072811667e91e1391
Jakub Urbański <kuba.urban01@gmail.com>
2023-01-03 18:26:11 +0100

    LOAN-46: add initial ef migration

---
+++ Src/Infrastructure/LoansComparer.DataPersistence/Migrations/20230103171948_Initialization.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoansComparer.DataPersistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialization : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Banks",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Domain = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostInquiryRoute = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GetInquiryRoute = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostOfferRoute = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GetOfferRoute = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostOfferDocumentRoute = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GetOfferDocumentRoute = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banks", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GovernmentId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GovernmentIdType = table.Column<int>(type: "int", nullable: false),
+                    BankID = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Users_Banks_BankID",
+                        column: x => x.BankID,
+                        principalTable: "Banks",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inquiries",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LoanValue = table.Column<int>(type: "int", nullable: false),
+                    NumberOfInstallments = table.Column<short>(type: "smallint", nullable: false),
+                    InquireDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()"),
+                    ChosenBankInquiryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BankID = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inquiries", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Inquiries_Banks_BankID",
+                        column: x => x.BankID,
+                        principalTable: "Banks",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_Inquiries_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inquiries_BankID",
+                table: "Inquiries",
+                column: "BankID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inquiries_UserID",
+                table: "Inquiries",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_BankID",
+                table: "Users",
+                column: "BankID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inquiries");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Banks");
+        }
+    }
+}
