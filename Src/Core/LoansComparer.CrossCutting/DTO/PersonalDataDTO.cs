06256c259a8e533cea7a18f4e68623f8a42e7dde
Jakub Urbański <kuba.urban01@gmail.com>
2023-01-04 00:53:34 +0100

    LOAN-31: update PersonalDataDTO

--- Src/Core/LoansComparer.CrossCutting/DTO/PersonalDataDTO.cs
+++ Src/Core/LoansComparer.CrossCutting/DTO/PersonalDataDTO.cs
@@ -6,6 +6,10 @@ public class PersonalDataDTO
 {
     public string FirstName { get; set; } = default!;
     public string LastName { get; set; } = default!;
+    public DateTime BirthDate { get; set; }
     public string GovernmentId { get; set; } = default!;
     public GovernmentIdType GovernmentIdType { get; set; }
+    public JobType JobType { get; set; }
+    public DateTime JobStartDate { get; set; }
+    public DateTime? JobEndDate { get; set; }
 }
