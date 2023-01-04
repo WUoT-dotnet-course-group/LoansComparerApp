8f783060fe82aa5c937dacbf6931c17ddf4949a7
Jakub Urbański <kuba.urban01@gmail.com>
2023-01-04 18:44:37 +0100

    LOAN-31: add loaning service

---
+++ Src/Core/Services/LoansComparer.Services/LoaningService.cs
@@ -0,0 +1,12 @@
+﻿using LoansComparer.Domain.Repositories;
+using LoansComparer.Services.Abstract;
+
+namespace LoansComparer.Services
+{
+    internal sealed class LoaningService : ILoaningService
+    {
+        private readonly IRepositoryManager _repositoryManager;
+
+        public LoaningService(IRepositoryManager repositoryManager) => _repositoryManager = repositoryManager;
+    }
+}
--- Src/Core/Services/LoansComparer.Services/ServiceManager.cs
+++ Src/Core/Services/LoansComparer.Services/ServiceManager.cs
@@ -7,15 +7,19 @@ namespace LoansComparer.Services
     {
         private readonly Lazy<IUserService> _lazyUserService;
         private readonly Lazy<IInquiryService> _lazyInquiryService;
+        private readonly Lazy<ILoaningService> _lazyLoaningService;
 
         public ServiceManager(IRepositoryManager repositoryManager, IServicesConfiguration configuration)
         {
             _lazyUserService = new Lazy<IUserService>(() => new UserService(repositoryManager, configuration));
             _lazyInquiryService = new Lazy<IInquiryService>(() => new InquiryService(repositoryManager));
+            _lazyLoaningService = new Lazy<ILoaningService>(() => new LoaningService(repositoryManager));
         }
 
         public IUserService UserService => _lazyUserService.Value;
 
         public IInquiryService InquiryService => _lazyInquiryService.Value;
+
+        public ILoaningService LoaningService => _lazyLoaningService.Value;
     }
 }
