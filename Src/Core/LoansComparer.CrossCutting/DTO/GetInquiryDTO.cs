ca7e0d1c9cd6dbe621a95786a23189456c884222
Jakub Urbański <kuba.urban01@gmail.com>
2023-01-05 23:09:53 +0100

    LOAN-45: remove redundant enum descriptions

--- Src/Core/LoansComparer.CrossCutting/DTO/GetInquiryDTO.cs
+++ Src/Core/LoansComparer.CrossCutting/DTO/GetInquiryDTO.cs
@@ -1,11 +1,9 @@
-﻿using LoansComparer.CrossCutting.Enums;
-
-namespace LoansComparer.CrossCutting.DTO;
+﻿namespace LoansComparer.CrossCutting.DTO;
 
 public class GetInquiryDTO
 {
     public int LoanValue { get; set; }
     public DateTime InquireDate { get; set; }
-    public string OfferStatus { get; set; } = Enums.OfferStatus.Unknown.GetEnumDescription();
+    public string OfferStatus { get; set; } = default!;
     public string ChosenBank { get; set; } = default!;
 }
--- Src/Core/LoansComparer.CrossCutting/Enums/OfferStatus.cs
+++ Src/Core/LoansComparer.CrossCutting/Enums/OfferStatus.cs
@@ -1,18 +1,11 @@
-﻿using System.ComponentModel;
-
-namespace LoansComparer.CrossCutting.Enums
+﻿namespace LoansComparer.CrossCutting.Enums
 {
     public enum OfferStatus
     {
-        [Description("Unknown")]
         Unknown = 0,
-        [Description("Uncompleted")]
         Uncompleted = 1,
-        [Description("Pending")]
         Pending = 2,
-        [Description("Accepted")]
         Accepted = 3,
-        [Description("Declined")]
         Declined = 4,
     }
 }
