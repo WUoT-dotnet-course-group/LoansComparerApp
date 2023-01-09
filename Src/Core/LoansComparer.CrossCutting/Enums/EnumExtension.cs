5232c41fa1c581d5d58d9e1f59e959f5fee671db
Jakub Urbański <kuba.urban01@gmail.com>
2023-01-09 15:59:38 +0100

    LOAN-47: add query extension for repository paging and sorting

--- Src/Core/LoansComparer.CrossCutting/Enums/EnumExtension.cs
+++ Src/Core/LoansComparer.CrossCutting/Enums/EnumExtension.cs
@@ -16,5 +16,24 @@ namespace LoansComparer.CrossCutting.Enums
 
             return enumType.GetField(fieldName)?.GetCustomAttributes(false).OfType<DescriptionAttribute>().SingleOrDefault()?.Description ?? string.Empty;
         }
+
+        public static bool TryGetEnumValue<T>(string? description, out T enumValue) where T : Enum
+        {
+            enumValue = default!;
+            
+            if (description == null)
+            {
+                return false;
+            }
+
+            var enumField = typeof(T).GetFields().SingleOrDefault(x => Attribute.GetCustomAttributes(x).OfType<DescriptionAttribute>().SingleOrDefault()?.Description == description);
+            var value = enumField?.GetValue(null);
+            if (value is not null) 
+            { 
+                enumValue = (T)value;
+            }
+            
+            return value is not null;
+        }
     }
 }
---
+++ Src/Infrastructure/LoansComparer.DataPersistence/Utils/DbInitializer.cs
@@ -0,0 +1,11 @@
+﻿namespace LoansComparer.DataPersistence.Utils
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(RepositoryDbContext dbContext)
+        {
+            dbContext.Database.EnsureCreated();
+            dbContext.SaveChanges();
+        }
+    }
+}
