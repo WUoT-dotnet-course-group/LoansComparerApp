6782a822ffeae820a8535d64203bcce5d1ef4a1e
Jakub Urbański <kuba.urban01@gmail.com>
2023-01-12 13:55:34 +0100

    LOAN-42: bind document upload to submission form

--- Src/LoansComparer.WebClient/src/app/inquire-process/inquiry-submission-form/document-upload/document-upload.component.ts
+++ Src/LoansComparer.WebClient/src/app/inquire-process/inquiry-submission-form/document-upload/document-upload.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  ControlContainer,
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  Validators,
+} from '@angular/forms';
 import { LoansComparerService } from 'src/app/shared/services/loans-comparer/loans-comparer.service';
 
 @Component({
   selector: 'app-document-upload',
   templateUrl: './document-upload.component.html',
   styleUrls: ['./document-upload.component.less'],
+  viewProviders: [
+    { provide: ControlContainer, useExisting: FormGroupDirective },
+  ],
 })
 export class DocumentUploadComponent implements OnInit {
-  constructor(private loansComparerService: LoansComparerService) {}
+  parentForm!: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(
+    private parent: FormGroupDirective,
+    private loansComparerService: LoansComparerService
+  ) {}
+
+  ngOnInit(): void {
+    this.parentForm = this.parent.form;
+    this.parentForm.addControl(
+      'document',
+      new FormControl<File | null>(null, [Validators.required])
+    );
+  }
 
   fileName = '';
   file: File | null = null;
@@ -17,6 +38,7 @@ export class DocumentUploadComponent implements OnInit {
   onFileSelected(event: any) {
     this.file = event.target.files[0];
     if (this.file) {
+      this.parentForm.patchValue({ document: this.file });
       this.fileName = this.file.name;
     }
   }
