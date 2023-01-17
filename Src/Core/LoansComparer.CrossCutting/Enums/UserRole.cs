e85b9f0fb295637711126a9442b2ad7ec0f20670
Jakub Urbański <kuba.urban01@gmail.com>
2023-01-17 16:07:45 +0100

    LOAN-40: add user role db column

---
+++ Src/Core/LoansComparer.CrossCutting/Enums/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace LoansComparer.CrossCutting.Enums
+{
+    public enum UserRole
+    {
+        Debtor = 0,
+        BankEmployee = 1
+    }
+}
--- Src/Core/LoansComparer.Domain/Entities/User.cs
+++ Src/Core/LoansComparer.Domain/Entities/User.cs
@@ -1,10 +1,13 @@
-﻿namespace LoansComparer.Domain.Entities
+﻿using LoansComparer.CrossCutting.Enums;
+
+namespace LoansComparer.Domain.Entities
 {
     public class User
     {
         public Guid ID { get; set; }
         public string? Email { get; set; }
         public PersonalData? PersonalData { get; set; }
+        public UserRole Role { get; set; }
 
         public Guid? BankID { get; set; }
         public virtual Bank? Bank { get; set; }
